be6ecbe81601b8e485d94f38374c9464ab6cb1d8
akinmode <akinjil@gmail.com>
2023-03-11 23:39:09 +0000

    Added 0x03x04

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """
+        Replaces an element in a list at a specific position
+        without modifying the original list (like in C).
+    """
+    my_listc = my_list.copy()
+
+    if idx < 0 or idx > len(my_listc):
+        return my_listc
+    else:
+        my_listc[idx] = element
+        return my_listc
