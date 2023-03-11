a6e1d44fd2dbf3fcbb0dc41fc725ee49e78d367e
akinmode <akinjil@gmail.com>
2023-03-11 23:58:12 +0000

    Fixed 0x03x02

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,8 +3,11 @@ def replace_in_list(my_list, idx, element):
     """
         Replaces an element of a list at a specific position (like in C).
     """
-    if idx < 0 or idx > len(my_list):
-        return my_list
+    if my_list is not None or idx is not None:
+        if idx < 0 or idx >= len(my_list):
+            return my_list
+        else:
+            my_list[idx] = element
+            return my_list
     else:
-        my_list[idx] = element
-        return my_list
+        return
