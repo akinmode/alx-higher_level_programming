4244ff3b32299c29aaf9ada599c4aa3be9e32267
akinmode <akinjil@gmail.com>
2023-03-12 17:02:24 +0000

    Added 0x03x08

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """
+        Returns a tuple with the length of a string and its first character.
+    """
+    if sentence is not None:
+        return (len(sentence), sentence[0])
+    else:
+        return None
