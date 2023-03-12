19d21b6ec6932b820795100b351a744de5334c29
Akinwale Akinjiola <akinjil@gmail.com>
2023-03-12 17:20:18 +0000

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         if row is not None and row != []:
             for col in row[:-1]:
-                print(col, end=' ')
-            print(row[-1])
+                print("{:d}".format(col), end=' ')
+            print("{:d}".format(row[-1]))
         else:
             print()
