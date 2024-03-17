ebcb1debe005853cb1d04179d6365d6ca7b5b5b7
akinmode <akinjil@gmail.com>
2024-03-17 21:16:20 +0000

    Added 11-delete

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    """
+        Deletes the item at a specific position in a list.
+    """
+    if my_list is not None or idx is not None:
+        if idx < 0 or idx >= len(my_list):
+            return my_list
+        else:
+            del my_list[idx]
+            return my_list
+    else:
+        return
