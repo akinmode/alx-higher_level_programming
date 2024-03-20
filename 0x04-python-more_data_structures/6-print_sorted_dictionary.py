b584bdf497d0839010442eb71e30d91a4d79d708
akinmode <akinjil@gmail.com>
2024-03-20 23:28:25 +0000

    Added 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    """
+        Prints a dictionary by ordered keys.
+    """
+    if a_dictionary is not None:
+        for x in sorted(a_dictionary):
+            print("{}: {}".format(x, a_dictionary[x]))
