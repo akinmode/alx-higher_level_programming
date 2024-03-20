3d9be1da4b6f4fbf5ee30ab6f53ff4515dcc8511
akinmode <akinjil@gmail.com>
2024-03-20 23:29:41 +0000

    Added 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """"
+        Replaces or adds key/value in a dictionary.
+    """
+    if a_dictionary is not None:
+        a_dictionary[key] = value
+        return a_dictionary
