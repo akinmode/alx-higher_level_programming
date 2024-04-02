c72621091d1b391e2b8c0895694757c843ac10e8
akinmode <akinjil@gmail.com>
2024-04-02 21:50:01 +0100

    Modified 103-magic

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,10 +3,10 @@ import math
 
 class _MagicClass:
     def __init__(self, radius):
-        self.__radius = 0  # Initialize __radius attribute
+        self.__radius = 0
         if type(radius) is not int or type(radius) is not float:
             raise TypeError('radius must be a number')
-        self.__radius = radius  # Set __radius attribute if radius is valid
+        self.__radius = radius
 
     def area(self):
         return self.__radius ** 2 * math.pi
