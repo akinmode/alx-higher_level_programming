cb8f2035661e00281801f35ee9a57d1cf9399947
akinmode <akinjil@gmail.com>
2024-04-15 21:42:10 +0100

    Added 6-base

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A BaseGeometry Class """
+
+
+class BaseGeometry:
+    """ A BaseGeometry class """
+    def area(self):
+        raise Exception("area() is not implemented")
