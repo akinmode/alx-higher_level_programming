68a77dd15cce5a7dcf6fdfd1b6ee32a0f0af3b20
akinmode <akinjil@gmail.com>
2024-04-16 18:58:14 +0100

    Added 10-student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Defines a student by name and age """
+
+
+class Student:
+    """ Defines a Student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        mydict = {}
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+
+                if attr in self.__dict__:
+                    mydict[attr] = self.__dict__[attr]
+
+            return mydict
+        return self.__dict__
